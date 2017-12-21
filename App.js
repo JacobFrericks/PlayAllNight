06e6ce37cca5bb47fc6b2ffa5975f5c2d8288695
Jacob Frericks <jacob.frericks@gmail.com>
2017-12-21 11:38:29 -0600

    Adding icons and onclick functions

--- App.js
+++ App.js
@@ -1,23 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Dimensions, StyleSheet, TouchableOpacity, View} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Device from 'react-native-device-detection';
+
+const deviceW = Dimensions.get('window').width;
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity activeOpacity={.5} onPress={this.onPressPlay}>
+                    <FontAwesome name="play" size={px2dp(50)} color="#fff" style={{marginRight: 20}}/>
+                </TouchableOpacity>
+                <TouchableOpacity activeOpacity={.5} onPress={this.onPressPause}>
+                    <FontAwesome name="pause" size={px2dp(50)} color="#fff" style={{marginRight: 20}}/>
+                </TouchableOpacity>
+                <TouchableOpacity activeOpacity={.5} onPress={this.onPressClock}>
+                    <FontAwesome name="clock-o" size={px2dp(50)} color="#fff" style={{marginRight: 10}}/>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
+    onPressPlay() {
+        console.log("PLAY");
+    }
+    onPressPause() {
+        console.log("PAUSE");
+    }
+    onPressClock() {
+        console.log("CLOCK");
+    }
+}
+
+function px2dp(px) {
+    let basePx;
+    if (Device.isTablet) {
+        basePx = 1000;
+    } else {
+        basePx = 375;
+    }
+    return px * deviceW / basePx
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    }
 });
